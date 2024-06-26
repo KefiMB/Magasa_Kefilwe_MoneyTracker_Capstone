7dead7082183f4a3a5146f3b9cd17f7934e5586a
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-06-25 21:25:02 -0400

    form

---
+++ money-tracker/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1>$400<span>.00</span></h1>
+      <form>
+        <input type="text" placeholder={'+200 nwe sanmsung tv'}/>
+        <input type="datetime-local"/>
+        <input type="text" placeholder={'description'}/>
+      </form>
+    </main>
+
+  );
+}
+
+export default App;
